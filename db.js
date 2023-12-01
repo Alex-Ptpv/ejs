8e4cb660971d6bbaddf795139aad40aeb4130387
Alex-Ptpv <oleksii.potapov@keyin.com>
2023-12-01 03:11:36 -0330

    test-db

--- db.js
+++ db.js
@@ -27,7 +27,66 @@ function getClient() {
   return client;
 }
 
+// db.js or config.js
+const sqlite3 = require('sqlite3');
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, 'test.db');
+
+const db = new sqlite3.Database(dbPath);
+
+const insertDataQuery = `
+  INSERT INTO customers (customer_id, customer_name, contact_name, address, city, postal_code, country)
+  VALUES (?, ?, ?, ?, ?, ?, ?);
+`;
+
+// Sample data
+const data = {
+  customer_id: '1244567',
+  customer_name: 'John Doe',
+  contact_name: 'John',
+  address: '123 Main St',
+  city: 'Cityville',
+  postal_code: '12345',
+  country: 'Countryland',
+};
+
+// Execute the query with parameters
+db.run(
+  insertDataQuery,
+  [
+    data.customer_id,
+    data.customer_name,
+    data.contact_name,
+    data.address,
+    data.city,
+    data.postal_code,
+    data.country,
+  ],
+  function (err) {
+    if (err) {
+      console.error('Error inserting data:', err.message);
+    } else {
+      console.log('Data inserted successfully asdda');
+    }
+  }
+);
+
+const selectDataQuery = 'SELECT * FROM customers';
+
+// Execute the query
+db.all(selectDataQuery, [], (err, rows) => {
+  if (err) {
+    console.error('Error selecting data:', err.message);
+  } else {
+    console.log('Selected data:', rows);
+  }
+});
+
 module.exports = {
   connect,
   getClient,
+  db
 };
+
+
